c40d4ee7c9056be1959115ce99673c0d50070382
heejung <8253jang@daum.net>
2022-08-30 23:42:15 +0900

    메인에서 수정함

--- src/kosta/mvc/admin/AdminController.java
+++ src/kosta/mvc/admin/AdminController.java
@@ -2,4 +2,8 @@ package kosta.mvc.admin;
 
 public class AdminController {
 
+	/**
+	 * 메인에서 수정
+	 * */
+	public void test() {}
 }
--- src/kosta/mvc/admin/AdminView.java
+++ src/kosta/mvc/admin/AdminView.java
@@ -1,5 +1,10 @@
 package kosta.mvc.admin;
 
 public class AdminView {
-
+  /**
+   * 메인에서 추가
+   * */
+	public void aaa() {
+		
+	}
 }
