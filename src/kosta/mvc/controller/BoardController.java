1587b430693966ce63ab13450c2bff2a7a16b6b7
장희정 <web18teacher@naver.com>
2022-08-30 22:53:08 +0900

    왼쪽 TestHeejung 충돌예상...

--- src/kosta/mvc/controller/BoardController.java
+++ src/kosta/mvc/controller/BoardController.java
@@ -1,5 +1,8 @@
 package kosta.mvc.controller;
 
 public class BoardController {
+	public void select() {
+		System.out.println("TestHeejung ผ๖มควิ...");
+	}
 
 }
--- src/kosta/mvc/controller/GoodsController.java
+++ src/kosta/mvc/controller/GoodsController.java
@@ -1,5 +1,7 @@
 package kosta.mvc.controller;
 
 public class GoodsController {
-
+   public void bb() {
+	   System.out.println("web18teacher ผ๖มควิ...");
+   }
 }
