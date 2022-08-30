ef6e1494e7e96b8973d25771382b1ff1b72e7b6b
heejung <8253jang@daum.net>
2022-08-30 22:54:45 +0900

    오른쪽 8253jang 충돌을 야기함..

--- src/kosta/mvc/controller/BoardController.java
+++ src/kosta/mvc/controller/BoardController.java
@@ -1,5 +1,7 @@
 package kosta.mvc.controller;
 
 public class BoardController {
-
+   public void insert() {
+	   System.out.println("8253jang ผ๖มควิ...");
+   }
 }
--- src/kosta/mvc/controller/ReplyController.java
+++ src/kosta/mvc/controller/ReplyController.java
@@ -2,6 +2,10 @@ package kosta.mvc.controller;
 
 public class ReplyController {
    public void replyInsert() {
+	   System.out.println("오른쪽 수저이ㅣ");
+   }
+   
+   public void aa() {
 	   
    }
 }
